3cad057ae8ad17c7ae365a6c7f57e36f461ee6e3
divyashasinha23 <divyashasinha23@gmail.com>
2020-11-26 20:41:06 +0530

    connected with database

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,21 @@
 const express = require("express");
+const connectDB = require('./config/db');
+const dotenv = require('dotenv');
+const colors = require('colors');
 
+dotenv.config();
+
+connectDB()
 const app = express();
+
+app.use(express.json());
+
+
  
 app.get("/", (req,res) => {
     res.send("Server running");
 });
 
-app.listen(5000, () =>{
-    console.log("server started at port 5000");
-});
+PORT = process.env.PORT;
+
+app.listen(PORT, ()=> console.log(`Server started at port ${PORT}`));
