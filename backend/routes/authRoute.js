87b0c5cd15f78bbe22a4e0a4c233b3f1f8474077
divyashasinha23 <divyashasinha23@gmail.com>
2020-12-01 17:25:03 +0530

    autenticated

---
+++ backend/routes/authRoute.js
@@ -0,0 +1,9 @@
+const {Router} = require('express');
+const authController =  require('../controllers/authControllers');
+
+const router = Router();
+
+router.post('/signup',authController.signup_post);
+router.post('/signin',authController.signin_post);
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 const colors = require('colors');
 const courseRoutes = require('./routes/courseRoutes');
 const courses = require('./data/courses');
+const authRoutes = require('./routes/authRoute'); 
 
 dotenv.config();
 
@@ -12,6 +13,7 @@ const app = express();
 
 app.use(express.json());
 app.use('/api/courses', courseRoutes);
+app.use(authRoutes);
 
  
 app.get("/", (req,res) => {
