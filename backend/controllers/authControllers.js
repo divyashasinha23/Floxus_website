eac0d6d5ec51444ab99b0381c6ede4e7b90236b5
nagendra0032 <nagendrakumar0032@gmail.com>
2020-12-07 23:38:05 +0530

    registration flow done

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,28 +1,57 @@
 const User = require('../models/userModel');
 const jwt = require('jsonwebtoken');
+const asyncHandler = require('express-async-handler');
 
 //json web token
 
- const createToken = (id) => {
-     return jwt.sign({id}, 'courseToken',{
-     expiresIn: '30d'
- });
+const createToken = (id) => {
+  return jwt.sign({ id }, 'courseToken', {
+    expiresIn: '30d',
+  });
 };
 
 //controller actions
 
-module.exports.signup_post  = async (req,res) => {
-    const {name, userName, email, password, phone } = req.body;
+// module.exports.signup_post = asyncHandler(async (req, res) => {
+//   const { name, userName, email, password, phone } = req.body;
 
-    try{
-        const user = await User.create({name, userName, email, password, phone});
-        const token = createToken(user._id);
-        res.status(200).json({success: true});
+//   const user = await User.create({ name, userName, email, password, phone });
+//   if (user) {
+//     res.status(201);
+//     res.json({
+//       _id: user._id,
+//       name: user.name,
+//       email: user.email,
+//       token: createToken(user._id),
+//     });
+//   } else {
+//     res.status(400);
+//     res.json({ message: 'error' });
+//   }
+// });
+
+module.exports.signup_post = async (req, res) => {
+  const { name, userName, email, password, phone } = req.body;
+
+  try {
+    const user = await User.create({ name, userName, email, password, phone });
+    if (user) {
+      res.status(201);
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        token: createToken(user._id),
+      });
+    } else {
+      res.status(400);
+      res.json({ message: 'error' });
     }
-    catch(err){
-        res.status(404).json({message: "error"});
-    }
-}
+  } catch (error) {
+    res.status(400);
+    res.json({ message: 'error' });
+  }
+};
 
 // module.exports.signin_post = async (req,res) => {
 //     const {email, password} = req.body;
@@ -37,7 +66,3 @@ module.exports.signup_post  = async (req,res) => {
 
 //     }
 // }
-
-
-
-
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,42 +1,41 @@
 const mongoose = require('mongoose');
-const {isEmail} = require('validator');
+const { isEmail } = require('validator');
 const bcrypt = require('bcrypt');
 
-const userSchema = new mongoose.Schema
-({
-    name: {
-        type: String,
-        required: [true, 'Name is Required']
-    },
-    userName:{
-        type: String,
-        required: true,
-        unique: true,
-    },
-    email:{
-        type: String,
-        required: [true, 'Email is Required'],
-        unique: true,
-        validate : [isEmail, 'Please enter a valid Email']
-    },
-    password:{
-        type: String,
-        required: [true, 'Password is Required']
-    },
-    phone:{
-        type: Number,
-        required: true,
-    },
-    isAdmin:{
-        type: Boolean,
-        required: true,
-    },
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is Required'],
+  },
+  userName: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is Required'],
+    unique: true,
+    validate: [isEmail, 'Please enter a valid Email'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is Required'],
+  },
+  phone: {
+    type: Number,
+    required: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    required: false,
+  },
 });
- userSchema.pre('save', async function(next){
-     const salt = await bcrypt.genSalt();
-     this.password = await bcrypt.hash(this.password,salt);
-     next();
-  });
+userSchema.pre('save', async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
 
 // //login user
 // userSchema.statics.signin = async function(email, password){
@@ -53,6 +52,6 @@ const userSchema = new mongoose.Schema
 
 // jwt
 
-const User = mongoose.model('User',userSchema);
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
