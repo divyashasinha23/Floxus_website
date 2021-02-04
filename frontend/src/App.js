2086092eeb1bf30ea3f817220b5afd7b141e5e63
divyashasinha23 <divyashasinha23@gmail.com>
2021-02-04 21:32:35 +0530

    certificate section added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,26 @@
 import CampusSection from './components/CampusSection/CampusSection';
+import CertificationSection from './components/CertificationSection/CertificationSection';
 import CourseSection from './components/CourseSection/CourseSection';
 import HeroSection from './components/HeroSection/HeroSection';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
     <div>
-      <HeroSection />
-      <CourseSection />
-      <CampusSection />
+     <Switch>
+       <Route path="/" exact>
+         <HeroSection />
+         <CourseSection />
+         <CampusSection />
+       </Route>
+       <Route path="/course">
+        <CertificationSection />
+       </Route>
+     </Switch>
+      
     </div>
+    </Router>
   );
 }
 
---
+++ frontend/src/components/CertificationSection/CertificationSection.js
@@ -0,0 +1,140 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+
+const CertificateWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  background-color: #3c378e;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CertificateContainer = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 80%;
+ 
+
+  @media (max-width: 496px) {
+    flex-direction: column;
+  }
+`;
+
+
+const CertificateData = styled.div`
+display: flex;
+flex-basis: 60%;
+height: 100vh;
+justify-content: center;
+align-items: center;
+`;
+
+const CertificateContent = styled.div`
+display: flex;
+flex-direction: column;
+height: 50%;
+width: 100%;
+
+@media (max-width: 496px) {
+  height: 80%;
+}
+`;
+
+const Heading = styled.div`
+font-size: 2.5em;
+color: white;
+font-weight: 410;
+letter-spacing: 1px;
+margin-bottom: 10px;
+
+@media (max-width: 496px) {
+  font-size: 2em;
+  letter-spacing: 0;
+}
+`;
+
+const Description = styled.div`
+color: #cecce3;
+font-size: 14px;
+font-weight: 300;
+padding-right: 20%;
+line-height: 25px;
+padding-top: 10px;
+letter-spacing: 0.5px;
+`;
+
+const Contact = styled.div`
+color: #fff;
+font-size: 15px;
+font-style: italic;
+fonr-weight: 400px;
+padding-right: 20%;
+line-height: 25px;
+letter-spacing: 0.5px; 
+padding-top: 50px;
+`;
+
+const CertificateButton = styled.div`
+display: flex;
+width: 150px;
+height: 40px;
+background: transparent linear-gradient(85deg, #ed841f 0%, #ff5400 100%) 0% 0%
+  no-repeat padding-box;
+border: none;
+border-radius: 22.5px;
+margin-top: 20px;
+justify-content: center;
+align-items: center;
+color: white;
+font-size: 14px;
+font-weight: 400;
+cursor: pointer;
+
+@media (max-width: 496px) {
+  height: 55px;
+}
+`;
+const CertificateImage = styled.div`
+background-color: white;
+width: 500px;
+height:230px;
+padding:20px;
+align-item: center;
+margin-top: 160px;
+`
+
+
+function CertificationSection() {
+    return (
+        <>
+            <CertificateWrapper>
+                <CertificateContainer>
+                 <CertificateData>
+                     <CertificateContent>
+                         <Heading>
+                             Certification
+                         </Heading>
+                         <Description>
+                         Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                         diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                         aliquyam.
+                         </Description>
+                         <Contact>
+                             We love taking your calls.
+                         </Contact>
+                         <CertificateButton>
+                             GET IN TOUCH
+                         </CertificateButton>
+                     </CertificateContent>
+                 </CertificateData>
+                 <CertificateImage>
+                 </CertificateImage>
+                </CertificateContainer>
+            </CertificateWrapper>
+        </>
+    )
+}
+
+export default CertificationSection
